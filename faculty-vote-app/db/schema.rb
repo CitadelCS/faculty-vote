4f384fd984b382e6cacbe3cbcd92646c79bf7edd
jmathews <jmathew1@citadel.edu>
2019-11-12 16:08:12 +0000

    added migration and initializer

--- faculty-vote-app/db/schema.rb
+++ faculty-vote-app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191031003508) do
+ActiveRecord::Schema.define(version: 20191112160127) do
 
   create_table "proposals", force: :cascade do |t|
     t.text     "name"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20191031003508) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"

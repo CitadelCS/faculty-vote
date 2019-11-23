3a31536e96bd1bca51312652cc2dd149fed7e479
EC2 Default User <john.thevos@gmail.com>
2019-11-23 16:52:34 +0000

    implemented sessions as a db table rather than using cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_faculty-vote-app_session'
+Rails.application.config.session_store :active_record_store, key: '_faculty-vote-app_session'
+
+# The below code can likely be deleted. This is a template just in case we need to handle sessions differently between prod and dev
+# if Rails.env == 'production'
+#     Rails.application.config.session_store :active_record_store, key: '_faculty-vote-app_session', domain: 'shielded-scrubland-95680.herokuapp.com/'
+# else
+#     Rails.application.config.session_store :active_record_store, key: '_faculty-vote-app_session'
+# end
